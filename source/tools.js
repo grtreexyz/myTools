7e31f6cb12d73f79575e64ad85b21d727fb888b0
栾树崇 <shuchong.luan@ywart.com>
2018-03-23 11:56:20 +0800

    1

--- source/tools.js
+++ source/tools.js
@@ -1,14 +1,19 @@
 /**
-     * 创建CSS样式表
-     * @param css
-     * @param cssDoc
-     */
-    function createStyle(css) {
-        var style = document.createElement("style");
-        style.type = "text/css";
-        document.getElementsByTagName("head")[0].appendChild(style);
-        if (style.styleSheet)
-            style.styleSheet.cssText = css;
-        else
-            style.appendChild(document.createTextNode(css));
-    }
+ * 创建CSS样式表
+ * @param css
+ * @param cssDoc
+ */
+function createStyle(css) {
+    var style = document.createElement("style");
+    style.type = "text/css";
+    document.getElementsByTagName("head")[0].appendChild(style);
+    if (style.styleSheet)
+        style.styleSheet.cssText = css;
+    else
+        style.appendChild(document.createTextNode(css));
+}
+
+
+function hexToRgba(hex, opacity) {
+    return "rgba(" + parseInt("0x" + hex.slice(1, 3)) + "," + parseInt("0x" + hex.slice(3, 5)) + "," + parseInt("0x" + hex.slice(5, 7)) + "," + opacity + ")";
+}
