0ab7870cf53c1dc83872cf1dc2f46670e64790c8
术虫 <35955204@qq.com>
2019-03-29 18:12:59 +0800

    Update cookie.js

--- source/cookie.js
+++ source/cookie.js
@@ -1,20 +1,17 @@
 ﻿//cookie操作
 _cookie = {
-    set: function(key, val, time) { //设置cookie方法
+    set: function (key, val, time) { //设置cookie方法
         var date = new Date(); //获取当前时间
-        if (time)
-            var expiresDays = time; //将date设置为n天以后的时间
-        else
-            var expiresDays = 2;
+        var expiresDays = time || 2;
         date.setTime(date.getTime() + expiresDays * 24 * 3600 * 1000); //格式化为cookie识别的时间
         document.cookie = key + "=" + val + ";expires=" + date.toGMTString(); //设置cookie
     },
-    get: function(key) { //获取cookie方法
+    get: function (key) { //获取cookie方法
         var temp = document.cookie.match(new RegExp("(?:^| )" + key + "=([^;]*)(?:;|$)"));
         if (temp != null) return temp[1];
         else return null;
     },
-    getAll: function() {
+    getAll: function () {
         var temp = document.cookie.match(/[^= ]*=[^;]*;/g);
         var r = {},
             t;
@@ -24,7 +21,7 @@ _cookie = {
         }
         return r;
     },
-    del: function(key) { //删除cookie方法
+    del: function (key) { //删除cookie方法
         var date = new Date(); //获取当前时间
         date.setTime(date.getTime() - 10000); //将date设置为过去的时间
         document.cookie = key + "=v; expires =" + date.toGMTString(); //设置cookie
